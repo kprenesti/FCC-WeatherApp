d75c26dd6ccad45af16b8b527a40e1665ec31b2f
kprenesti <krista.prenesti@gmail.com>
2016-02-02 11:37:51 -0500

    Fixing and making bugs

--- js/ajax.js
+++ js/ajax.js
@@ -1,40 +1,131 @@
 $(document).ready(function() {
-  beginCallNow();
-  function beginCallNow(){
-      var latitude, longitude;
-      if(navigator.geolocation){
-        navigator.geolocation.getCurrentPosition(function(Position){
-          latitude = Position.coords.latitude;
-          longitude = Position.coords.longitude;
-          console.log('latitude: ' + latitude, 'longitude: '+ longitude);
-          ajaxWithGeo(latitude, longitude);
-        });
-      } else {
-        $('.bodywrap').prepend('Please enter a zipcode or City Name for current weather information.');
+  $('.promptCity').hide();
+
+  $('#country').change(function(){
+    if($('#country option:selected').attr('value') !== 'US'){
+      $('#states').hide();
+    } else {
+      $('#states').show();
+    }
+  })
+
+  $('#weather_status_form').submit(function(event){
+    getWeatherFromForm();
+    event.preventDefault();
+    // alert('It works!');
+  }); //end submit form
+
+  geolocation();
+
+  //called from within Ready
+  function geolocation(){
+    if(navigator.geolocation){
+      navigator.geolocation.getCurrentPosition(getWeatherFromGeo); //end geolocation lookup
+    } else {
+      $('.promptCity').show();
+    } //end if geolocation
+  } //end geolocation
+
+  //Called from within Ready and SubmitForm
+  function getWeatherFromForm(){
+    console.log('-------------------');
+    var req_url = "http://api.wunderground.com/api/11e9326259bb14c8/conditions/forecast/q/";
+    var req_params, city, state, country;
+    if($('#country option:selected').attr('value') === 'US'){
+      if($('#location').val() !== '' && $('#states option:selected').attr('value') !== 'Nope'){
+        state = $('#states option:selected').attr('value');
+        city = $('#location').val();
+        req_params = state + "/" + city;
+      } else if ($('#location').val() !== '' && $('#states option:selected').attr('value') === 'Nope') {
+        req_params = city;
+        console.log("No state was selected");
       }
-  } //end beginCallNow
+    } else {
+      country = $('#country option:selected').attr('value');
+      city = $('#location').val();
+      req_params = country +"/"+city;
+    }
+    var json_req = encodeURI(req_url+req_params+".json");
+    console.log(json_req);
+    $.getJSON( json_req, recieveData);
+  } //end getWeatherFromForm
 
-  function ajaxWithGeo(latitude, longitude){
-    $.ajax({
-      url: "http://api.openweathermap.org/data/2.5/forecast?units=imperial",
-      method: "get",
-      data: {
-        APPID: "f07943ab2e5d89848ac5de98ea9d55f2",
-        lat: latitude,
-        lon: longitude
+  //Called from within Ready
+  function getWeatherFromGeo(position){
+    // console.log(position);
+    console.log('-------------------');
+    //$("#forecastHolder").hide();
+    var req_url = "http://api.wunderground.com/api/11e9326259bb14c8/conditions/forecast/q/";
+    var req_params = position.coords.latitude + "," + position.coords.longitude;
+    console.log(req_params);
+    var json_req = encodeURI(req_url+req_params+".json");
+    console.log(json_req);
+    $.getJSON( json_req, recieveData);
+  } //end getWeatherFromForm
+
+  //Called from within getJSON
+  function recieveData(data){
+    // console.log(data);
+    var kristaData = {
+      city: data.current_observation.display_location.full,
+      temp: {
+        degF:[data.current_observation.temp_f,data.current_observation.feelslike_f],
+        degC:[data.current_observation.temp_c,data.current_observation.feelslike_c],
       },
-      dataType: "json",
-      success: function(data){
-        var imageSource = 'http://openweathermap.org/img/w/' + data.list[0].weather[0].icon + '.png';
-        console.log(imageSource);
-        $('.cityName').html(data.city.name + '\'s');
-        $('#weatherNow .icon').attr('src', imageSource);
-        $('span.unit').html($('#myform input[name=units]:checked').val());
-        $('#weatherNow .currentDegrees').html(data.list[0].main.temp);
+      today: {
+        lastObserved: data.current_observation.observation_time,
+        icon: data.current_observation.icon,
+        url: data.current_observation.icon_url,
+        weather: data.current_observation.weather
       },
-    }); //end .ajax;
+      forecast: data.forecast.txt_forecast.forecastday = [],
+    }; //end kristaData
+    fillInFields(kristaData);
+    // console.log(kristaData);
+  }
 
-  } //end ajaxWithGeo
+  //Called from within recieveData
+  function fillInFields(kristaData){
+    console.log(kristaData);
+    var todayTemp, feelsLikeTemp, forecastText, i;
+    var completeForecastDiv = "";
+    // console.log(forecastArray);
 
+      if($('input#unitSwitcher').is(':checked')){
+        todayTemp = kristaData.temp.degC[0];
+        feelsLikeTemp = kristaData.temp.degC[1];
+        $('.degree').text('C');
+      } else {
+        todayTemp = kristaData.temp.degF[0];
+        feelsLikeTemp = kristaData.temp.degF[1];
+        $('.degree').text('F');
+      }
 
-}) //end ready
+    console.log(todayTemp, feelsLikeTemp, forecastText);
+    $('.updated').html(kristaData.today.lastObserved);
+    $('.cityName').text(kristaData.city + "'s ");
+    $('#presently >.icon').attr('src',kristaData.today.url);
+    $('.temp-actual').text(todayTemp);
+    $('.condition').text(kristaData.today.weather);
+    $('.temp-feelsLike').text(feelsLikeTemp);
+
+    //-------Begin building Forecast section----------//
+
+    for(i=0; i<kristaData.forecast.length; i+=2){
+      if($('input#unitSwitcher').is(':checked')){
+        forecastText = kristaData.forecast[i].fcttxt_metric;
+      } else {
+        forecastText = kristaData.forecast[i].fcttxt;
+      }
+      console.log(forecastText);
+      completeForecastDiv += "<div class='dayHolder'><br>";
+      completeForecastDiv += "<div class='AM'><br>";
+      completeForecastDiv += "<h2 class='title'>"+kristaData.forecast[i].title+"</h2>";
+      completeForecastDiv += "<img src='"+ kristaData.forecast[i].icon_url +"' alt='"+ kristaData.forecast[i].icon +"'><br>";
+      completeForecastDiv += "<p class='weatherDesc'>"+forecastText+"</p>";
+      completeForecastDiv += "</div><!--end AM day"+i+"-->";
+    } //end for loop
+    return $('#forecastHolder').html(completeForecastDiv);
+  } //end buildForecast
+
+}); //end ready
--- js/main.js
+++ js/main.js
@@ -2,39 +2,54 @@ $(document).ready(function(){
   var theDate = new Date()
   var dateDisplay = theDate.toDateString;
   var location = "";
-  beginCallNow();
+  askForGeoloc();
 
-function beginCallNow(){
+  function askForGeoloc(){
     var latitude, longitude;
     if(navigator.geolocation){
       navigator.geolocation.getCurrentPosition(function(Position){
         latitude = Position.coords.latitude;
         longitude = Position.coords.longitude;
         var query = latitude + ',' + longitude;
+        console.log(query);
         ajaxRequest(query);
       });
     } else {
       $('.promptCity').css('display', 'block');
     }
-} //end beginCallNow
+  } //end beginCallNow
 
+  $('#subForm').click(function(){
+    var cityName = $('#city').val();
+    if(cityName.indexOf(/\s/g !== -1)){
+      cityName.replace(/\s/g, "_");
+    } else if(cityName.indexOf(/[^A-z\s]/g) !== -1){
+      cityName.replace(/[^A-z\s]/g, "");
+    }
+    console.log(cityName);
+    var query = $('#states').val() + "\/" + cityName;
 
-//Weather Underground KeyID: 11e9326259bb14c8
-function ajaxRequest(query){
-  $.ajax({
-    url: "http://api.wunderground.com/api/11e9326259bb14c8/conditions/forecast/geolookup/q/" + query + ".json",
-    method: "get",
-    data: {
-      units: auto,
-    },
-    success: function(data){
-      var imageSource = 'http://openweathermap.org/img/w/' + data.list[0].weather[0].icon + '.png';
-      console.log(imageSource);
-      $('.cityName').html(data.city.name + '\'s');
-      $('#weatherNow .icon').attr('src', imageSource);
-      $('span.unit').html($('#myform input[name=units]:checked').val());
-      $('#weatherNow .currentDegrees').html(data.list[0].main.temp);
+  }); //end form click
+
+  //Weather Underground KeyID: 11e9326259bb14c8
+  function ajaxRequest(query){
+    $.ajax({
+      url: "http://api.wunderground.com/api/11e9326259bb14c8/conditions/forecast/geolookup/q/" + query + ".json",
+      method: "get",
+      data: {
+        units: auto,
       },
-    }); //end .ajax;
-  } //end ajaxWithGeo
+      success: function(response){
+        var imageSource = "http://icons.wxug.com/i/c/i/" + response.current_observation.icon + ".gif";
+        console.log(response.current_observation.display_location.full);
+        $('.cityName').html(response.current_observation.display_location.full + '\'s');
+        $('#weatherNow .icon').attr('src', imageSource);
+        $('span.unit').html($('#myform input[name=units]:checked').val());
+        $('#weatherNow .currentDegrees').html(data.list[0].main.temp);
+        },
+      }).fail(function(){
+        $('#todaysWeather').html("<h2>Weather information is currently unavailable.  Please try again later</h2>");
+      }); //end .ajax;
+    } //end ajaxWithGeo
+
 }); //end ready
